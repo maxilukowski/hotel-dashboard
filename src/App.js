9bd2da27a47c674e79ef9ae65b08fce9b5091281
maxilukowski <maxi.lukowski@yahoo.com>
2021-01-20 15:15:02 +0100

    Dnd complete

--- src/App.js
+++ src/App.js
@@ -1,37 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import IncomingDeliveriesCard from './components/incomingDeliveries/IncomingDeliveriesCard'
 import OrderVolumeCard from './components/orderVolume/OrderVolumeCard'
 import SuppliersRankingCard from './components/suppliersRanking/SuppliersRankingCard'
 import TopThreeCard from './components/topThree/TopThreeCard'
-import { DragDropContext } from 'react-beautiful-dnd'
-import styled from 'styled-components'
 
 const cardComponents = [
   {
     cardName: <TopThreeCard />,
-    id: 1,
+    id: '1',
   },
   {
     cardName: <SuppliersRankingCard />,
-    id: 2,
+    id: '2',
   },
   {
     cardName: <OrderVolumeCard />,
-    id: 3,
+    id: '3',
   },
   {
     cardName: <IncomingDeliveriesCard />,
-    id: 4,
+    id: '4',
   },
 ]
 
 function App() {
+  const [cards, setCards] = useState(cardComponents)
+
+  function handleOnDragEnd(result) {
+    if (!result.destination) return
+    const items = [...cards]
+    const [reorderedItem] = items.splice(result.source.index, 1)
+    items.splice(result.destination.index, 0, reorderedItem)
+    setCards(items)
+  }
   return (
     <>
-      <DragDropContext>
-        {cardComponents.map(({ cardName, id }) => {
-          return <Wrapper key={id}>{cardName}</Wrapper>
-        })}
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId='dashboard-cards'>
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {cards.map(({ cardName, id }, index) => {
+                return (
+                  <Draggable key={id} draggableId={id} index={index}>
+                    {(provided) => (
+                      <Wrapper
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                      >
+                        {cardName}
+                      </Wrapper>
+                    )}
+                  </Draggable>
+                )
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
       </DragDropContext>
     </>
   )
