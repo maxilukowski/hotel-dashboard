58f4453f00c7818ba4f31e7d1f6c4c7495827cad
maxilukowski <maxi.lukowski@yahoo.com>
2021-01-20 14:39:04 +0100

    Install dnd-lib and map components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import IncomingDeliveriesCard from './components/incomingDeliveries/IncomingDeli
 import OrderVolumeCard from './components/orderVolume/OrderVolumeCard'
 import SuppliersRankingCard from './components/suppliersRanking/SuppliersRankingCard'
 import TopThreeCard from './components/topThree/TopThreeCard'
+import { DragDropContext } from 'react-beautiful-dnd'
+
+const cardComponents = [
+  {
+    cardName: <TopThreeCard />,
+    id: 1,
+  },
+  {
+    cardName: <SuppliersRankingCard />,
+    id: 2,
+  },
+  {
+    cardName: <OrderVolumeCard />,
+    id: 3,
+  },
+  {
+    cardName: <IncomingDeliveriesCard />,
+    id: 4,
+  },
+]
 
 function App() {
   return (
     <>
-      <TopThreeCard />
-      <SuppliersRankingCard />
-      <OrderVolumeCard />
-      <IncomingDeliveriesCard />
+      <DragDropContext>
+        {cardComponents.map(({ cardName, id }) => {
+          return <div key={id}>{cardName}</div>
+        })}
+      </DragDropContext>
     </>
   )
 }
