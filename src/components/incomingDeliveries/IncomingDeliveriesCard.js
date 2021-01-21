9afc1ccce64794e10e0b89deac5955248c59c9ac
maxilukowski <maxi.lukowski@yahoo.com>
2021-01-21 21:04:18 +0100

    Deliveries sorted by date and by supplier (raw)

--- src/components/incomingDeliveries/IncomingDeliveriesCard.js
+++ src/components/incomingDeliveries/IncomingDeliveriesCard.js
@@ -11,10 +11,46 @@ const IncomingDeliveriesCard = ({ data }) => {
     (a, b) => a.deliveryDate - b.deliveryDate
   )
 
+  let deliveriesByDayBySupplier = {}
+  sortedData.forEach(({ deliveryDate, supplier, productName, quantity }) => {
+    if (!deliveriesByDayBySupplier.hasOwnProperty(deliveryDate)) {
+      deliveriesByDayBySupplier[deliveryDate] = {}
+    }
+    if (!deliveriesByDayBySupplier[deliveryDate].hasOwnProperty(supplier)) {
+      deliveriesByDayBySupplier[deliveryDate][supplier] = []
+    }
+    deliveriesByDayBySupplier[deliveryDate][supplier].push({
+      productName,
+      quantity,
+    })
+  })
+
   return (
     <>
-      {sortedData.map(({ deliveryDate }, index) => {
-        return <div key={index}>{deliveryDate.toDateString()}</div>
+      {Object.keys(deliveriesByDayBySupplier).map((deliveryDate, index) => {
+        return (
+          <div key={index}>
+            {new Date(deliveryDate).toDateString()}
+            {Object.keys(deliveriesByDayBySupplier[deliveryDate]).map(
+              (supplier, index) => {
+                return (
+                  <div key={index}>
+                    {supplier}:
+                    {deliveriesByDayBySupplier[deliveryDate][supplier].map(
+                      ({ productName, quantity }, index) => {
+                        return (
+                          <div key={index}>
+                            {productName} - {quantity}
+                          </div>
+                        )
+                      }
+                    )}
+                  </div>
+                )
+              }
+            )}
+          </div>
+        )
       })}
     </>
   )
