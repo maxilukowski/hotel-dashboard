adb860cce8231b1ec52d371a15f657b7012ed170
maxilukowski <maxi.lukowski@yahoo.com>
2021-01-22 19:38:10 +0100

    Refactor SuppliersRankingCard & add price ranking

---
+++ src/components/suppliersRanking/SuppliersRankingByPrice.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Bar } from 'react-chartjs-2'
+
+const SuppliersRankingByPrice = ({ topThreeSupplier }) => {
+  return (
+    <div>
+      <Bar
+        data={{
+          labels: Object.keys(topThreeSupplier),
+          datasets: [
+            {
+              label: 'Total Price',
+              data: [
+                topThreeSupplier['Yum Food'].totalPrice,
+                topThreeSupplier['Booz drinks'].totalPrice,
+                topThreeSupplier['Allstuff supplies'].totalPrice,
+              ],
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+              ],
+              borderWidth: 1,
+            },
+          ],
+        }}
+        options={{
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+        }}
+      />
+    </div>
+  )
+}
+
+export default SuppliersRankingByPrice
--- src/components/suppliersRanking/SuppliersRankingCard.js
+++ src/components/suppliersRanking/SuppliersRankingCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Bar } from 'react-chartjs-2'
+import SuppliersRankingByQuantity from './SuppliersRankingByQuantity'
+import SuppliersRankingByPrice from './SuppliersRankingByPrice'
 
 const SuppliersRankingCard = ({ data }) => {
   let topThreeSupplier = {}
@@ -18,43 +19,8 @@ const SuppliersRankingCard = ({ data }) => {
 
   return (
     <Wrapper>
-      <Bar
-        data={{
-          labels: Object.keys(topThreeSupplier),
-          datasets: [
-            {
-              label: 'by amount',
-              data: [
-                topThreeSupplier['Yum Food'].totalQuantity,
-                topThreeSupplier['Booz drinks'].totalQuantity,
-                topThreeSupplier['Allstuff supplies'].totalQuantity,
-              ],
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-              ],
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-              ],
-              borderWidth: 1,
-            },
-          ],
-        }}
-        options={{
-          scales: {
-            yAxes: [
-              {
-                ticks: {
-                  beginAtZero: true,
-                },
-              },
-            ],
-          },
-        }}
-      />
+      <SuppliersRankingByQuantity topThreeSupplier={topThreeSupplier} />
+      <SuppliersRankingByPrice topThreeSupplier={topThreeSupplier} />
     </Wrapper>
   )
 }
